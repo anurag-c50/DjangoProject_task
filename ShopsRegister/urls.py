0516f7e1b5c087fbcf2c891dc0fdc41cca6948f3
ajjha5244@gmail.com <105697066+anurag-c50@users.noreply.github.com>
2024-10-20 06:17:45 +0530

    second commit

--- ShopsRegister/urls.py
+++ ShopsRegister/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns=[
-    path("register/",Registers),
+    path("register",Registers),
     path("userSearch",UserSerach),
-    path("list",listofshop)
+    path('', ShopListCreateView.as_view()),
+    path('search', ShopSearchView.as_view()),
+
 ]
--- ShopsRegister/views.py
+++ ShopsRegister/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
 from .registerform import Shopform
 from .userSearchForm import UserSearch
 from .models import Register
 from django import forms
-from rest_framework.decorators import api_view
+from rest_framework import serializers
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 from .serializers import Shopserializer
 from math import radians,cos,sin,sqrt,atan2
-# Create your views here.
 def clean_latitude(latitude):
     if latitude < -90 or latitude > 90:
         raise forms.ValidationError('Latitude must be between -90 and 90 degrees.')
@@ -18,17 +18,50 @@ def clean_longitude(longitude):
     if longitude < -180 or longitude > 180:
         raise forms.ValidationError('Longitude must be between -180 and 180 degrees.')
     return longitude
-@api_view(['GET'])
-def listofshop(request):
-    Shops=Register.objects.all()
-    Serializer=Shopserializer(Shops,many=True)
-    return Response(Serializer.data)
-# @api_view(['POST'])
-# def listofshop(request):
-#     Shops=Register.objects.all()
-#     Serializer=Shopserializer(Shops,many=True)
-#     return Response(Serializer.data)
+def validate(data):
+    if data['Latitude'] == 0 and data['Longitude'] == 0:
+        raise serializers.ValidationError("Latitude and Longitude cannot both be zero.")
+    elif data['Longitude'] < -180 or data['Longitude'] > 180:
+        raise serializers.ValidationError("Longitude must be between -180 and 180.")
+    elif data['Latitude']  < -90 or data['Latitude']  > 90:
+        raise serializers.ValidationError('Latitude must be between -90 and 90 degrees.')
+    return data
+class ShopListCreateView(APIView):
+    def get(self, request):
+        shop = Register.objects.all()
+        serializer = Shopserializer(shop, many=True)
+        return Response(serializer.data)
 
+    def post(self, request):
+        validate(data=request.data)
+        serializer=Shopserializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ShopSearchView(APIView):
+    def haversine_distance(self, lat1, lon1, lat2, lon2):
+     R = 6371
+     differencelat = radians(lat2 - lat1)
+     differencelon = radians(lon2 - lon1)
+     A = sin(differencelat / 2) ** 2 + cos(radians(lat1)) * cos(radians(lat2)) * sin(differencelon / 2) ** 2
+     C = 2 * atan2(sqrt(A), sqrt(1 - A))
+     return R * C
+
+    def post(self, request):
+        latitude = request.data['latitude']
+        longitude = request.data['longitude']
+        Shops = Register.objects.all()
+
+        ShopDistances = []
+        for shop in Shops:
+            distance = self.haversine_distance(latitude, longitude, shop.Latitude, shop.Longitude)
+            ShopDistances.append({"name": shop.name, "address": shop.address, "distance": distance})
+
+        sorted_shops = sorted(ShopDistances, key=lambda x: x['distance'])
+        return Response(sorted_shops)
+      
 def Registers(request):
     if request.method=='POST':
         form=Shopform(request.POST)
